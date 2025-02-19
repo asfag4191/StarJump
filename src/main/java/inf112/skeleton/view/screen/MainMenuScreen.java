d982429aaa935743e28ca46a3ff0c59f079b4237
Jonas <jojus3401@uib.no>
2025-02-19 14:33:26 +0100

    Added - click-sound when pressing buttons. - sample sounds for button click
    Changed
    - Title from "Welcome to StarJump!" to "StarJump"
    - Reformatted MainMenuScreen::createUI for better code readability

--- src/main/java/inf112/skeleton/view/screen/MainMenuScreen.java
+++ src/main/java/inf112/skeleton/view/screen/MainMenuScreen.java
@@ -1,7 +1,10 @@
 package inf112.skeleton.view.screen;
 
+import inf112.skeleton.model.StarJump;
+
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
@@ -11,13 +14,13 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import inf112.skeleton.model.StarJump;
 
 public class MainMenuScreen implements Screen {
     final StarJump game;
     final Texture background;
     private final Stage stage;
     private final Skin skin;
+    private final Sound buttonClick;
 
 
     public MainMenuScreen(StarJump game) {
@@ -26,10 +29,10 @@ public class MainMenuScreen implements Screen {
         this.stage = new Stage(this.game.viewport);
         Gdx.input.setInputProcessor(this.stage);
 
-        /*
-        TO CHANGE FONT OF TITLE AND BUTTONSTYLE, CHANGE THIS SKIN ONLY
-         */
+        /*  TO CHANGE FONT OF TITLE AND BUTTONSTYLE, CHANGE THIS SKIN ONLY  */
         this.skin = new Skin(Gdx.files.internal("src/main/assets/skins/craftacular/skin/craftacular-ui.json"));
+        /*  TO CHANGE BUTTON CLICK AUDIO, CHANGE THIS  */
+        this.buttonClick = Gdx.audio.newSound(Gdx.files.internal("src/main/assets/audio/sounds/buttonClick-short.mp3"));
     }
 
     @Override
@@ -68,7 +71,9 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        this.stage.dispose();
+        this.skin.dispose();
+        this.background.dispose();
     }
 
     private void draw() {
@@ -101,29 +106,42 @@ public class MainMenuScreen implements Screen {
         float buttonX = viewport.getWorldWidth() / 2f - buttonWidth / 2;
         float buttonY = viewport.getWorldHeight() / 2f;
 
-        // Create button
+        // Create buttons
         TextButton startButton = new TextButton("Start Game", skin);
-        TextButton optionsButton = new TextButton("Options", skin);
-        TextButton quitButton = new TextButton("Quit", skin);
         startButton.setSize(buttonWidth, buttonHeight);
-        optionsButton.setSize(buttonWidth, buttonHeight);
-        quitButton.setSize(buttonWidth, buttonHeight);
         startButton.setPosition(buttonX, buttonY);
+
+        TextButton optionsButton = new TextButton("Options", skin);
+        optionsButton.setSize(buttonWidth, buttonHeight);
         optionsButton.setPosition(buttonX, buttonY - buttonHeight - buttonGap);
+
+        TextButton quitButton = new TextButton("Quit", skin);
+        quitButton.setSize(buttonWidth, buttonHeight);
         quitButton.setPosition(buttonX, buttonY - 2*buttonHeight - 2*buttonGap);
 
-        // Add button click event
+        // Add buttons click events
         startButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
+                buttonClick.play(1.0f);
                 game.setScreen(new GameScreen(game)); // Switch to GameScreen
                 dispose();
             }
         });
+
         quitButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
+                buttonClick.play(1.0f);
                 Gdx.app.exit();
+            }
+        });
+
+        optionsButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                buttonClick.play(1.0f);
+                System.out.println("OPTIONS NOT YET IMPLEMENTED");
             }
         });
 
@@ -136,8 +154,8 @@ public class MainMenuScreen implements Screen {
     private void drawTitle(Viewport viewport) {
         Label.LabelStyle titleStyle = new Label.LabelStyle();
         titleStyle.font = this.skin.getFont("title");
-
-        Label titleLabel = new Label("Welcome to StarJump!", titleStyle);
+        titleStyle.font.getData().setScale(1.6f);
+        Label titleLabel = new Label("StarJump", titleStyle);
         titleLabel.setPosition(viewport.getWorldWidth() / 2 - titleLabel.getWidth() / 2,
                 viewport.getWorldHeight() - (viewport.getWorldHeight() / 3.3f));
         stage.addActor(titleLabel);
