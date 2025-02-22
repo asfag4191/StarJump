b1e78ee5f646bfba86d0371ddbd2b8ecc28bdbd2
Mohamad Al-Mozzen <mohalm1299@gmail.com>
2025-02-22 17:21:17 +0100

    - Done with Animator.java

--- src/main/java/inf112/skeleton/view/Animator.java
+++ src/main/java/inf112/skeleton/view/Animator.java
@@ -2,18 +2,28 @@ package inf112.skeleton.view;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
 
 import java.util.HashMap;
 
 public class Animator {
-    private HashMap<String, Animation<TextureRegion>> animations;
+    private final HashMap<String, Animation<TextureRegion>> animations;
+    private Animation<TextureRegion> currentAnimation;
+    private String currentKey;
     private float currentTime;
-    private boolean isPlaying;
     private boolean isPaused;
 
-    public void createAnimation(String key, Texture animationSheet, int rows, int cols, float fps) {
+    public Animator() {
+     this.animations = new HashMap<>();
+    }
+
+    public void addAnimation(String key, Animation<TextureRegion> anim) {
+        animations.put(key, anim);
+    }
+
+    public void addAnimation(String key, Texture animationSheet, int rows, int cols, float fps) {
         TextureRegion[][] temp = TextureRegion.split(animationSheet,
                 animationSheet.getWidth()/cols,
                 animationSheet.getHeight()/rows);
@@ -26,30 +36,43 @@ public class Animator {
         }
 
         Animation<TextureRegion> anim = new Animation<>(1/fps, animFrames);
-        animations.put(key, anim);
+        addAnimation(key, anim);
     }
 
-    public TextureRegion updateAnimation(String key, float dt) {
-        if (!isPlaying) {
-            currentTime = 0;
-            isPlaying = true;
-            isPaused = false;
-        }
+    public void addAnimation(String key, TextureAtlas atlas, String regionName, float fps) {
+        Array<TextureAtlas.AtlasRegion> frames = atlas.findRegions(regionName);
+        Animation<TextureRegion> anim = new Animation<>(1/fps, frames);
+        addAnimation(key, anim);
+    }
 
-        if (!isPaused) currentTime += dt;
+    public void play(String key) {
+        if (key == null) throw new RuntimeException("the key must be none null");
+        if (key.equals(currentKey)) return;
 
-        return animations.get(key).getKeyFrame(currentTime, true);
+        currentAnimation = animations.get(key);
+        currentKey = key;
+        currentTime = 0;
+        isPaused = false;
+    }
+
+    public void stop() {
+        currentKey = null;
     }
 
     public void pause() {
         isPaused = true;
     }
 
-    public boolean isPlaying() {
-        return isPlaying;
+    public TextureRegion update(float dt) {
+        if (!isPaused) currentTime += dt;
+        return currentAnimation.getKeyFrame(currentTime, true);
     }
 
+    public boolean isPlaying() {
+        return currentKey != null;
+    }
 
-
-
+    public boolean isPaused() {
+        return isPaused;
+    }
 }
