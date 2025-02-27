8f418d2c41ec1decccd58032ff0c4b05ccec83fd
Thora Moen <thmoe8353@uib.no>
2025-02-27 11:01:47 +0100

    started the keyListener in PlayerController, implementing iControllableGameModel. Tests in PlayerControllerTest

--- src/main/java/inf112/skeleton/controller/PlayerController.java
+++ src/main/java/inf112/skeleton/controller/PlayerController.java
@@ -1,4 +1,58 @@
 package inf112.skeleton.controller;
 
-public class PlayerController {
+import java.awt.event.KeyEvent;
+
+import com.badlogic.gdx.backends.lwjgl3.audio.Mp3.Sound;
+
+import inf112.skeleton.model.GameState;
+import inf112.skeleton.model.StarJump;
+import inf112.skeleton.model.character.Character;
+import inf112.skeleton.view.screen.GameScreen;
+import inf112.skeleton.view.screen.MainMenuScreen;
+
+/**
+ * this class will move the player with keystrokes
+ */
+public class PlayerController implements java.awt.event.KeyListener {
+    final StarJump game;
+    Character player;
+    GameScreen gameScreen;
+    private Sound jumpSound; // sound when player jumps, add later
+    private iControllableGameModel controller;
+
+    public PlayerController(StarJump game, inf112.skeleton.model.character.Character player,
+            iControllableGameModel controller) {
+        this.game = game;
+        this.controller = controller;
+        this.player = player;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (controller.getGameState() == GameState.GAME_ACTIVE) {
+            if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                controller.movePlayer(0, -1);
+            } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                controller.movePlayer(0, 1);
+            } else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+                controller.movePlayer(2, 0);
+            } else if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                // player use powerUp
+            } else if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                game.setScreen(new MainMenuScreen(game));
+            }
+
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // empty for now
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // empty for now
+    }
+
 }
--- src/main/java/inf112/skeleton/controller/iControllableGameModel.java
+++ src/main/java/inf112/skeleton/controller/iControllableGameModel.java
@@ -1,4 +1,40 @@
 package inf112.skeleton.controller;
 
+import inf112.skeleton.model.GameState;
+
 public interface iControllableGameModel {
+    /**
+     * moves the player y a spesified number of rows and columns, if possible
+     * 
+     * @param row the row that is moved
+     * @param col the column that is moved
+     * @return true if move is possible
+     */
+    boolean movePlayer(int row, int col);
+
+    /**
+     * moves the player upwards if possible
+     * 
+     * @return the new position
+     */
+    boolean upPlayer();
+
+    /**
+     * @return the game state
+     *         Active Game
+     *         Game Over
+     */
+    GameState getGameState();
+
+    /**
+     * setting the game state
+     * 
+     * @param gameState, the new game state
+     */
+    void setGameState(GameState gameState);
+
+    /**
+     * @return the speed of the jumping player
+     */
+    int getSpeed();
 }
