9ae221e315bd285430f6c19663a0bce1e08393d7
Ase Fagerbakke <ase.fagerbakke@student.uib.no>
2025-03-25 16:49:48 +0100

    Need to fix the tests, fixed some javadocs

--- src/main/java/inf112/skeleton/model/items/powerup/DiamondPowerUp.java
+++ src/main/java/inf112/skeleton/model/items/powerup/DiamondPowerUp.java
@@ -7,16 +7,25 @@ import inf112.skeleton.model.character.controllable_characters.Player;
 
 /**
  * A power-up representing a collectible diamond that increases the player's score.
+ * 
+ * When collected, this power-up adds a fixed score value to the global score.
+ * This class implements {@link iPowerUp} for unified interaction with the power-up system.
  */
 public class DiamondPowerUp implements iPowerUp {
-
-    private final Player player;
+   private final Player player;
     private final Sprite sprite;
     private final Vector2 position;
     private final int scoreValue = 10;
     private static int score;
 
 
+     /**
+     * Constructs a DiamondPowerUp.
+     *
+     * @param player   The player collecting the diamond.
+     * @param position The position where the diamond appears.
+     * @param sprite   The graphical representation of the diamond.
+     */
     public DiamondPowerUp(Player player, Vector2 position, Sprite sprite) {
         this.player = player;
         this.position = position;
@@ -24,15 +33,24 @@ public class DiamondPowerUp implements iPowerUp {
         this.sprite.setPosition(position.x, position.y);
         this.score=score;
     }
-
+    /**
+     * Increases the global score by the specified value.
+     *
+     * @param value The amount of score to add.
+     */
     public void addScore(int value) {
         score += value;
     }
 
+    /**
+     * Gets the current score accumulated from all diamonds.
+     *
+     * @return The total diamond score.
+     */
     public static int getScore() {
         return score;
     }
-    
+
     @Override
     public void applyPowerUpEffect() {
         addScore(scoreValue);
