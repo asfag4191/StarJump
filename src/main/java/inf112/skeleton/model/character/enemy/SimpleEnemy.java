837cdfc9d30294b9cca060d5e04a8c752c3e91f9
Jonas <jojus3401@uib.no>
2025-04-29 15:40:15 +0200

    Removed unused code

--- src/main/java/inf112/skeleton/model/character/enemy/SimpleEnemy.java
+++ src/main/java/inf112/skeleton/model/character/enemy/SimpleEnemy.java
@@ -9,20 +9,11 @@ import inf112.skeleton.model.character.Character;
 public abstract class SimpleEnemy {
     public final Character enemyCharacter;
     public World world;
-    private Vector2 target;
 
     public SimpleEnemy(Character enemy, World world) {
         this.enemyCharacter = enemy;
         this.world = world;
         setupAnimation();
-    }
-
-    public Vector2 getTarget() {
-        return target;
-    }
-
-    public void setTarget(Vector2 point) {
-        target = point;
     }
 
     public void update(float dt) {
