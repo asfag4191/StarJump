68da3cff92fdc454174ff8230158cd80b8e8ad2f
Jonas <jojus3401@uib.no>
2025-04-29 15:43:00 +0200

    Removed dead code

--- src/main/java/inf112/skeleton/model/character/enemy/SimpleEnemy.java
+++ src/main/java/inf112/skeleton/model/character/enemy/SimpleEnemy.java
@@ -1,7 +1,6 @@
 package inf112.skeleton.model.character.enemy;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 
 import inf112.skeleton.model.character.Character;
